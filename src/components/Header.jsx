6870497bf433a2d2224d74d85dc909f2d786ec71
mkreynolds919 <mkreynolds919@gmail.com>
2025-08-12 17:44:50 -0500

    style: Readjust header title to be left-aligned

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,16 +7,14 @@ export default function Header() {
     return (
         <header>
             <section className="top-line">
-                <div className="empty"></div>
-                <h1 className="title">
-                    <Link className="title link-reset" to="/">Seflora</Link>
-                </h1>
+                <Link className="title link-reset" to="/">Seflora</Link>
                 <Link to="cart" className="link-reset">
                     <img className="basket" src={basketIcon}></img>
                 </Link>
             </section>
             <nav>
                 <Link className="link-reset nav-link" to="collections/beauty">Beauty</Link>
+                <Link className="link-reset nav-link" to="collections/skin-care">Skincare</Link>
                 <Link className="link-reset nav-link" to="collections/fragrances">Fragrances</Link>
             </nav>
         </header>
