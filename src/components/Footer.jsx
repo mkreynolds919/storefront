c411ea5e735fea093c27138169ba6cdbca86b745
mkreynolds919 <mkreynolds919@gmail.com>
2025-08-12 17:22:50 -0500

    feat: Add rendered Footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,9 @@
-export default function Footer() {}
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className="support"><b>Have a question?</b> support@seflora.com</div>
+            <div className="credits">Icons from <a href="https://icons8.com/" className="link-reset">Icons8</a>.</div>
+        </footer>
+    );
+}
