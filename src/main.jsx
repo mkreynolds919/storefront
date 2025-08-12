d638c54ea86731d3a2b3015272eb11e7b4933ead
mkreynolds919 <mkreynolds919@gmail.com>
2025-08-12 10:52:06 -0500

    feat: Create skeleton for App component

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
 import routes from "./routes.js";
+import App from "./App.jsx";
 
 const router = createBrowserRouter(routes);
 
