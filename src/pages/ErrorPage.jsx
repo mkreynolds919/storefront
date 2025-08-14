42cb1e747888e7b5584fd7d7a3fb57603701240e
mkreynolds919 <mkreynolds919@gmail.com>
2025-08-14 15:37:40 -0500

    feat: Introduce ErrorPage for error handling

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,2 +1,9 @@
+import '../styles/ErrorPage.css';
 
-export default function ErrorPage() {}
+export default function ErrorPage() {
+    return(
+        <div className="error-container">
+            <h1 className="error-header">Could not load. Try refreshing or navigating away.</h1>
+        </ div>
+    );
+}
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,10 +1,12 @@
 import { useParams } from 'react-router-dom';
 import useItem from "../hooks/useItem.js";
 import LoadingProductDetail from './LoadingProductDetail.jsx';
+import ErrorPage from "./ErrorPage.jsx";
 
 export default function ProductDetail() {
     const id = useParams().id;
     const { data, loading, error } = useItem(id);
 
     if (loading) return <LoadingProductDetail />
+    if (error) return <ErrorPage />
 }
