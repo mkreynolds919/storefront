52ce6138d026a51fefbf8b0680283e798c22afa8
mkreynolds919 <mkreynolds919@gmail.com>
2025-08-14 16:35:50 -0500

    feat: Complete visual elements of ProductDetail for individual items

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import useItem from "../hooks/useItem.js";
 import LoadingProductDetail from './LoadingProductDetail.jsx';
 import ErrorPage from "./ErrorPage.jsx";
+import "../styles/ProductDetail.css";
 
 export default function ProductDetail() {
     const id = useParams().id;
@@ -9,4 +10,22 @@ export default function ProductDetail() {
 
     if (loading) return <LoadingProductDetail />
     if (error) return <ErrorPage />
+
+    return (
+        <div className="product-detail">
+            <div className="product-detail-image">
+                <img src={data.images[0]} height='600px' width='600px'></img>
+            </div>
+            <div className="product-text-group">
+                <div className="product-top-line">
+                    <div className="product-detail-title">{data.title}</div>
+                    <div className="product-detail-brand">{data.brand}</div>
+                </div>
+                <div className="product-detail-description">{data.description}</div>
+                <div className="product-bottom-line">
+                    <div className="product-detail-price">${data.price}</div>
+                </div>
+            </div>
+        </div>
+    )
 }
