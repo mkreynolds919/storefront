a2b0a3243cf55dcc588d6cc8bb310b07119c19f1
mkreynolds919 <mkreynolds919@gmail.com>
2025-08-17 13:48:11 -0500

    feat: Create cart adding elements and mutate data into a larger object per item

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,4 +1,5 @@
 import { useParams, useOutletContext } from 'react-router-dom';
+import { useState } from 'react';
 import useItem from "../hooks/useItem.js";
 import LoadingProductDetail from './LoadingProductDetail.jsx';
 import ErrorPage from "./ErrorPage.jsx";
@@ -6,16 +7,22 @@ import "../styles/ProductDetail.css";
 
 export default function ProductDetail() {
     const id = useParams().id;
-    const { setCart } = useOutletContext();
+    const { cart, setCart } = useOutletContext();
+    const [quantity, setQuantity] = useState(1);
     const { data, loading, error } = useItem(id);
 
     function addToCart(id, num) {
         setCart(prevCart => {
             const newCart = { ...prevCart };
             if (newCart[id]) {
-                newCart[id] += num;
+                newCart[id].setQuantity = parseInt(newCart[id].quantity || 0) + parseInt(num);
             } else {
-                newCart[id] = num;
+                newCart[id] = {
+                    name: data.title,
+                    brand: data.brand,
+                    price: data.price,
+                    quantity: parseInt(num),
+                }
             }
             return newCart;
         });
@@ -23,7 +30,7 @@ export default function ProductDetail() {
 
     if (loading) return <LoadingProductDetail />
     if (error) return <ErrorPage />
-
+    console.log(cart);
     return (
         <div className="product-detail">
             <div className="product-detail-image">
@@ -37,6 +44,16 @@ export default function ProductDetail() {
                 <div className="product-detail-description">{data.description}</div>
                 <div className="product-bottom-line">
                     <div className="product-detail-price">${data.price}</div>
+                    <div className="add-to-cart">
+                        <button className="add-to-cart-button" onClick={() => addToCart(id, quantity)}>Add to cart</button>
+                        <select className="quantity-select" onChange={(e) => setQuantity(e.target.value)}>
+                            {Array.from({ length: 8}, (_, i) => {
+                                return (
+                                    <option key={i+1} value={i+1}>{i+1}</option>
+                                )
+                            })}
+                        </select>
+                    </div>
                 </div>
             </div>
         </div>
