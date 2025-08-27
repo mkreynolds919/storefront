4660ba49c8547045fa4f4835cbe832126deabd4c
mkreynolds919 <mkreynolds919@gmail.com>
2025-08-27 14:52:53 -0500

    feat: Style and structure cart products

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,23 +6,34 @@ export default function Cart() {
     const { cart, setCart } = useOutletContext();
     console.log(cart); 
     return (
-        <div className="cart-container">
-            {Object.keys(cart).length === 0 
-                ? <div className="empty-cart">Oops! Your cart is empty.</div> 
-                : Object.entries(cart).map((item) => {
-                    console.log(item);
-                    return <CartProductCard 
-                                key={item[0]} 
-                                title={item[1].name} 
-                                brand={item[1].brand} 
-                                price={item[1].price} 
-                                quantity={item[1].quantity} 
-                                image={item[1].image} 
-                                id={item[0]} 
-                                setCart={setCart} 
-                            />
-            })}
-        </div>
-    )
-
+        <>
+            <div className="cart-header">Shopping Cart</div>
+            <div className="cart-container">
+                {Object.keys(cart).length === 0 
+                    ? <div className="empty-cart">Oops! Your cart is empty.</div>
+                    :  <div className="cart-products">
+                            {Object.entries(cart).map((item) => {
+                                console.log(item);
+                                return ( 
+                                    <>
+                                        <CartProductCard 
+                                            key={item[0]} 
+                                            title={item[1].name} 
+                                            brand={item[1].brand} 
+                                            price={item[1].price} 
+                                            quantity={item[1].quantity} 
+                                            image={item[1].image} 
+                                            id={item[0]} 
+                                            setCart={setCart} 
+                                        />
+                                        <hr/>
+                                    </>
+                                )
+                                })}
+                        </div>
+                }
+            </div>
+        </>
+        
+    );
 }
